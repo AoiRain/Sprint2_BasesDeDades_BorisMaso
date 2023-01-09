e00c7a6589fcd7f764b927ff372831f471c36f99
AoiRain <113428090+AoiRain@users.noreply.github.com>
2023-01-09 03:36:22 +0100

    Entrega2.3 nivell1 tenda i pizzeria sense diagrama

--- Entrega2.3_MongodbEstructura_BorisMaso/Entrega2.3_Optica_BorisMaso.js
+++ Entrega2.3_MongodbEstructura_BorisMaso/Entrega2.3_Optica_BorisMaso.js
@@ -1,170 +1,23 @@
 db.dropDatabase();
-db.createCollection('OpticaCulDampolla');
-db.createCollection('Comandes');
-db.OpticaCulDampolla.insertOne(
+db.createCollection('Proveidors');
+db.createCollection('Clients');
+db.Proveidors.insertOne(
     {
-        "Proveidors": [
-            {
-                "Nom": "Ulleres Ripastro",
-                "Adreca": {
-                    "Carrer": "Guinoa",
-                    "Numero": 23,
-                    "Pis": 4,
-                    "Ciutat": "Barcelona",
-                    "CP": "08045",
-                    "Pais": "Espana"
-                },
-                "Telefon": "636453421",
-                "Fax": "934563782",
-                "NIF": "A12059812",
-                "Producte": [
-                    {
-                        "Marca": "Garivaldi",
-                        "Graduacio": {
-                            "VidreEsquerre": 0.25,
-                            "VidreDret": 0.25
-                        },
-                        "Montura": "Flotant",
-                        "Color": "Verd",
-                        "ColorVidre": {
-                            "VidreEsquerre": "Verd",
-                            "VidreDret": "Verd"
-                        },
-                        "Preu": 150
-                    },
-                    {
-                        "Marca": "Burvundi",
-                        "Graduacio": {
-                            "VidreEsquerre": -0.25,
-                            "VidreDret": -0.25
-                        },
-                        "Montura": "Metalica",
-                        "Color": "Negre",
-                        "ColorVidre": {
-                            "VidreEsquerre": "Gris",
-                            "VidreDret": "Gris"
-                        },
-                        "Preu": 200
-                    }
-                ]
-            },
-            {
-                "Nom": "Vidres Nika",
-                "Adreca": {
-                    "Carrer": "Guimpo",
-                    "Numero": 194,
-                    "Pis": 1,
-                    "Porta": 1,
-                    "Ciutat": "Barcelona",
-                    "CP": "08034",
-                    "Pais": "Espana"
-                },
-                "Telefon": "632124567",
-                "NIF": "B12009861",
-                "Producte": [
-                    {
-                        "Marca": "Velvet",
-                        "Graduacio": {
-                            "VidreEsquerre": 0.50,
-                            "VidreDret": 0.50
-                        },
-                        "Montura": "Pasta",
-                        "Color": "Burdeus",
-                        "ColorVidre": {
-                            "VidreEsquerre": "Vermell",
-                            "VidreDret": "Vermell"
-                        },
-                        "Preu": 180
-                    },
-                    {
-                        "Marca": "Giovanni",
-                        "Graduacio": {
-                            "VidreEsquerre": -0.50,
-                            "VidreDret": -0.50
-                        },
-                        "Montura": "Metalica",
-                        "Color": "Platejat",
-                        "ColorVidre": {
-                            "VidreEsquerre": "transparent",
-                            "VidreDret": "Transparent"
-                        },
-                        "Preu": 130
-                    }
-                ]
-            }
-        ],
-        "Clients": [
-            {
-                "Nom": "Roberto Fuentes Amigo",
-                "Adreca": {
-                    "Carrer": "Vallverde",
-                    "Numero": 18,
-                    "Pis": 4,
-                    "Porta": 1,
-                    "Ciutat": "Barcelona",
-                    "CP": "08018",
-                    "Pais": "Espana"
-                },
-                "CorreuElectronic": "robofont@hotmail.com",
-                "DataRegistre": {"$date": "2022-10-22"}
-            },
-            {
-                "Nom": "Paula Miro Albino",
-                "Adreca": {
-                    "Carrer": "Rocagran",
-                    "Numero": 98,
-                    "Pis": 1,
-                    "Porta": 1,
-                    "Ciutat": "Barcelona",
-                    "CP": "08014",
-                    "Pais": "Espana"
-                },
-                "CorreuElectronic": "plmr@gmail.com",
-                "DataRegistre": {"$date": "2022-11-16"},
-                "NomClientRecomendador": "Roberto Fuentes Amigo"
-            }
-        ]
-
-    }
-);
-db.Comandes.insertMany(
-    [
-        {
-            "Producte": {
-                "Marca": "Velvet",
-                "Graduacio": {
-                    "VidreEsquerre": 0.50,
-                    "VidreDret": 0.50
-                },
-                "Montura": "Pasta",
-                "Color": "Burdeus",
-                "ColorVidre": {
-                    "VidreEsquerre": "Vermell",
-                    "VidreDret": "Vermell"
-                },
-                "Preu": 180,
-                "Quantitat": 1
-            },
-            "PreuTotal": 180,
-            "Client": {
-                "Nom": "Roberto Fuentes Amigo",
-                "Adreca": {
-                    "Carrer": "Vallverde",
-                    "Numero": 18,
-                    "Pis": 4,
-                    "Porta": 1,
-                    "Ciutat": "Barcelona",
-                    "CP": "08018",
-                    "Pais": "Espana"
-                },
-                "CorreuElectronic": "robofont@hotmail.com",
-                "DataRegistre": {"$date": "2022-10-22"}
-            },
-            "Treballador": "Jordi Stanuz Ribagorsa",
-            "DataVenda": {"$date": "2022-10-24"}
+        "Nom": "Ulleres Ripastro",
+        "Adreca": {
+            "Carrer": "Guinoa",
+            "Numero": 23,
+            "Pis": 4,
+            "Ciutat": "Barcelona",
+            "CP": "08045",
+            "Pais": "Espana"
         },
-        {
-            "Producte": {
+        "Telefon": "636453421",
+        "Fax": "934563782",
+        "NIF": "A12059812",
+        "Producte": [
+            {
+                "_id": "1",
                 "Marca": "Garivaldi",
                 "Graduacio": {
                     "VidreEsquerre": 0.25,
@@ -176,28 +29,119 @@ db.Comandes.insertMany(
                     "VidreEsquerre": "Verd",
                     "VidreDret": "Verd"
                 },
-                "Preu": 150,
-                "Quantitat": 1
+                "Preu": 150
             },
-            "PreuTotal": 150,
-            "Client": {
-                "Nom": "Paula Miro Albino",
-                "Adreca": {
-                    "Carrer": "Rocagran",
-                    "Numero": 98,
-                    "Pis": 1,
-                    "Porta": 1,
-                    "Ciutat": "Barcelona",
-                    "CP": "08014",
-                    "Pais": "Espana"
+            {
+                "_id": "2",
+                "Marca": "Burvundi",
+                "Graduacio": {
+                    "VidreEsquerre": -0.25,
+                    "VidreDret": -0.25
                 },
-                "CorreuElectronic": "plmr@gmail.com",
-                "DataRegistre": {"$date": "2022-11-16"},
-                "NomClientRecomendador": "Roberto Fuentes Amigo"
+                "Montura": "Metalica",
+                "Color": "Negre",
+                "ColorVidre": {
+                    "VidreEsquerre": "Gris",
+                    "VidreDret": "Gris"
+                },
+                "Preu": 200
+            }
+        ]
+    }
+);
+db.Proveidors.insertOne(
+    {
+        "Nom": "Vidres Nika",
+        "Adreca": {
+            "Carrer": "Guimpo",
+            "Numero": 194,
+            "Pis": 1,
+            "Porta": 1,
+            "Ciutat": "Barcelona",
+            "CP": "08034",
+            "Pais": "Espana"
+        },
+        "Telefon": "632124567",
+        "NIF": "B12009861",
+        "Producte": [
+            {
+                "_id": "3",
+                "Marca": "Velvet",
+                "Graduacio": {
+                    "VidreEsquerre": 0.50,
+                    "VidreDret": 0.50
+                },
+                "Montura": "Pasta",
+                "Color": "Burdeus",
+                "ColorVidre": {
+                    "VidreEsquerre": "Vermell",
+                    "VidreDret": "Vermell"
+                },
+                "Preu": 180
             },
+            {
+                "_id": "4",
+                "Marca": "Giovanni",
+                "Graduacio": {
+                    "VidreEsquerre": -0.50,
+                    "VidreDret": -0.50
+                },
+                "Montura": "Metalica",
+                "Color": "Platejat",
+                "ColorVidre": {
+                    "VidreEsquerre": "transparent",
+                    "VidreDret": "Transparent"
+                },
+                "Preu": 130
+            }
+        ]
+    }
+);
+db.Clients.insertOne(
+    {
+        "Nom": "Roberto Fuentes Amigo",
+        "Adreca": {
+            "Carrer": "Vallverde",
+            "Numero": 18,
+            "Pis": 4,
+            "Porta": 1,
+            "Ciutat": "Barcelona",
+            "CP": "08018",
+            "Pais": "Espana"
+        },
+        "CorreuElectronic": "robofont@hotmail.com",
+        "DataRegistre": {"$date": "2022-10-22"},
+        "NomClientRecomendador": "",
+        "Comandes": {
+            "Producte_id": "1",
+            "Quantitat": "1",
+            "PreuTotal": "150",
+            "Treballador": "Jordi Stanuz Ribagorsa",
+            "DataVenda": {"$date": "2022-10-24"}
+        }
+    }
+);
+db.Clients.insertOne(
+    {
+        "Nom": "Paula Miro Albino",
+        "Adreca": {
+            "Carrer": "Rocagran",
+            "Numero": 98,
+            "Pis": 1,
+            "Porta": 1,
+            "Ciutat": "Barcelona",
+            "CP": "08014",
+            "Pais": "Espana"
+        },
+        "CorreuElectronic": "plmr@gmail.com",
+        "DataRegistre": {"$date": "2022-11-16"},
+        "NomClientRecomendador": "Roberto Fuentes Amigo",
+        "Comandes": {
+            "Producte_id": "3",
+            "Quantitat": "1",
+            "PreuTotal": "180",
             "Treballador": "Laia Vilanova Cami",
             "DataVenda": {"$date": "2022-11-18"}
         }
-        
-    ]
+    }
 );
--- Entrega2.3_MongodbEstructura_BorisMaso/Entrega2.3_Pizzeria_BorisMaso.js
+++ Entrega2.3_MongodbEstructura_BorisMaso/Entrega2.3_Pizzeria_BorisMaso.js
@@ -1,333 +1,200 @@
 db.dropDatabase();
-db.createCollection('Locals');
-db.createCollection('Comandes');
-db.Locals.insertMany(
-    [
-        {
-            "Nom": "Pizzeria Valken1",
-            "Adreca": {
-                "Carrer": "Avinguda de Madrid",
-                "Numero": 78,
-                "Pis": 0,
-                "Localitat": "Barcelona",
-                "Provincia": "Barcelona",
-                "CP": "08019",
-                "Pais": "Espana"
-            },
-            "Telefon": "934907643",
-            "NIF": "A12459892",
-            "Producte": [
-                
-                {
-                    "Nom": "Pizza barbacoa",
-                    "Descripció": "Tomaquet, orenga, formatge, carn picada i salsa barbacoa",
-                    "Imatge": "www.imatgepizza1.com",
-                    "Preu": 10,
-                    "Categoria": "Carn"
-                },
-                {
-                    "Nom": "Pizza quatre formatges",
-                    "Descripció": "Tomaquet, orenga, formatge blau, de cabra, de mozzarella i gorgonzola",
-                    "Imatge": "www.imatgepizza2.com",
-                    "Preu": 10,
-                    "Categoria": "SenseCarn"
-                },
-                {
-                    "Nom": "Pizza vegana",
-                    "Descripció": "Tomaquet, orenga, xampinyons, alberginies i salsa vegana",
-                    "Imatge":"www.imatgepizza3.com",
-                    "Preu": 10,
-                    "Categoria": "Vegana"
-                },
-                {
-                    "Nom": "Haburguesa americana",
-                    "Descripció": "Carn, formatge, tomaquet, ou ferrat, enciam i maionesa",
-                    "Imatge": "www.imatgehamburguesa1.com",
-                    "Preu": 8,
-                    "Categoria": "Carn"
-                },
-                {
-                    "Nom": "Hamburguesa vegetariana",
-                    "Descripció": "Carn beyond, formatge, tomaquet, enciam i salsa de iogurt",
-                    "Imatge": "www.imatgehamburguesa2.com",
-                    "Preu": 8,
-                    "Categoria": "SenseCarn"
-                },
-                {
-                    "Nom": "Hamburguesa Vegana",
-                    "Descripció": "Carn beyond, tomaquet, enciam, alberginia i salsa vegana",
-                    "Imatge": "www.imatgehamburguesa3.com",
-                    "Preu": 8,
-                    "Categoria": "Vegana"
-                },
-                {
-                    "Nom": "Coca-cola",
-                    "Descripció": "Beguda de cola amb gas",
-                    "Imatge": "www.imatgebeguda1",
-                    "Preu": 2
-                },
-                {
-                    "Nom": "Aigua",
-                    "Descripció": "Aigua de manantial",
-                    "Imatge": "www.imatgebeguda2",
-                    "Preu": 2
-                }
-            ],
-            "Treballadors": [
-                {
-                    "Nom": "Jordi",
-                    "Cognoms": "Cami Valldaura",
-                    "NIF": "46578905F",
-                    "Telefon": "456328967",
-                    "Posicio": "cuiner"
-                },
-                {
-                    "Nom": "Maura",
-                    "Cognoms": "Amadi Vilanova",
-                    "NIF": "45634577S",
-                    "Telefon": "456789765",
-                    "Posició": "Repartidor"
-                }
-            ],
-            "Clients": [
-                {
-                    "Nom": "Laura",
-                    "Cognoms": "Garriga Vilafont",
-                    "Adreca": {
-                        "Carrer": "Avinguda del Cubi",
-                        "Numero": 23,
-                        "Pis": 5,
-                        "Localitat": "Barcelona",
-                        "Provincia": "Barcelona",
-                        "CP": "08017",
-                        "Pais": "Espana"
-                    },
-                    "Telefon": "934907643"
-                }
-            ]
+db.createCollection('Tendes');
+db.createCollection('Productes');
+db.createCollection('Clients');
+db.Tendes.insertOne(
+    {
+        "_id": "1",
+        "Nom": "Pizzeria Valken1",
+        "Adreca": {
+            "Carrer": "Avinguda de Madrid",
+            "Numero": 78,
+            "Pis": 0,
+            "Localitat": "Barcelona",
+            "Provincia": "Barcelona",
+            "CP": "08019",
+            "Pais": "Espana"
         },
-        {
-            "Nom": "Pizzeria Valken2",
-            "Adreca": {
-                "Carrer": "Viladomat",
-                "Numero": 12,
-                "Pis": 0,
-                "Localitat": "Girona",
-                "Provincia": "Girona",
-                "CP": "17003",
-                "Pais": "Espana"
+        "Telefon": "934907643",
+        "NIF": "A12459892",
+        "Treballadors": [
+            {
+                "_id": "1",
+                "Nom": "Jordi",
+                "Cognoms": "Cami Valldaura",
+                "NIF": "46578905F",
+                "Telefon": "456328967",
+                "Posicio": "cuiner"
             },
-            "Telefon": "934976545",
-            "NIF": "B12089872",
-            "Producte": [
-                
-                {
-                    "Nom": "Pizza barbacoa",
-                    "Descripció": "Tomaquet, orenga, formatge, carn picada i salsa barbacoa",
-                    "Imatge": "www.imatgepizza1.com",
-                    "Preu": 10,
-                    "Categoria": "Carn"
-                },
-                {
-                    "Nom": "Pizza quatre formatges",
-                    "Descripció": "Tomaquet, orenga, formatge blau, de cabra, de mozzarella i gorgonzola",
-                    "Imatge": "www.imatgepizza2.com",
-                    "Preu": 10,
-                    "Categoria": "SenseCarn"
-                },
-                {
-                    "Nom": "Pizza vegana",
-                    "Descripció": "Tomaquet, orenga, xampinyons, alberginies i salsa vegana",
-                    "Imatge":"www.imatgepizza3.com",
-                    "Preu": 10,
-                    "Categoria": "Vegana"
-                },
-                {
-                    "Nom": "Haburguesa americana",
-                    "Descripció": "Carn, formatge, tomaquet, ou ferrat, enciam i maionesa",
-                    "Imatge": "www.imatgehamburguesa1.com",
-                    "Preu": 8,
-                    "Categoria": "Carn"
-                },
-                {
-                    "Nom": "Hamburguesa vegetariana",
-                    "Descripció": "Carn beyond, formatge, tomaquet, enciam i salsa de iogurt",
-                    "Imatge": "www.imatgehamburguesa2.com",
-                    "Preu": 8,
-                    "Categoria": "SenseCarn"
-                },
-                {
-                    "Nom": "Hamburguesa Vegana",
-                    "Descripció": "Carn beyond, tomaquet, enciam, alberginia i salsa vegana",
-                    "Imatge": "www.imatgehamburguesa3.com",
-                    "Preu": 8,
-                    "Categoria": "Vegana"
-                },
-                {
-                    "Nom": "Coca-cola",
-                    "Descripció": "Beguda de cola amb gas",
-                    "Imatge": "www.imatgebeguda1",
-                    "Preu": 2
-                },
-                {
-                    "Nom": "Aigua",
-                    "Descripció": "Aigua de manantial",
-                    "Imatge": "www.imatgebeguda2",
-                    "Preu": 2
-                }
-            ],
-            "Treballadors": [
-                {
-                    "Nom": "Ismael",
-                    "Cognoms": "Gutierrez Guimo",
-                    "NIF": "45321566Y",
-                    "Telefon": "659348721",
-                    "Posicio": "cuiner"
-                },
-                {
-                    "Nom": "Leila",
-                    "Cognoms": "Yock Lima",
-                    "NIF": "45732400A",
-                    "Telefon": "654321976",
-                    "Posició": "Repartidor"
-                }
-            ],
-            "Clients": [
-                {
-                    "Nom": "Sergi",
-                    "Cognoms": "Viladomat Herrera",
-                    "Adreca": {
-                        "Carrer": "Avinguda de Madrid",
-                        "Numero": 78,
-                        "Pis": 0,
-                        "Localitat": "Girona",
-                        "Provincia": "Girona",
-                        "CP": "17005",
-                        "Pais": "Espana"
-                    },
-                    "Telefon": "639286547"
-                }
-            ]
-        }
-    ]
-);
-db.Comandes.insertMany(
-    [
-        {
-            "DataComanda": {"$date": "2022-10-18"},
-            "Producte": [
-                {
-                    "Nom": "Hamburguesa vegetariana",
-                    "Descripció": "Carn beyond, formatge, tomaquet, enciam i salsa de iogurt",
-                    "Imatge": "www.imatgehamburguesa2.com",
-                    "Preu": 8,
-                    "Categoria": "SenseCarn",
-                    "Quantitat": 2
-                },
-                {
-                    "Nom": "Coca-cola",
-                    "Descripció": "Beguda de cola amb gas",
-                    "Imatge": "www.imatgebeguda1",
-                    "Preu": 2,
-                    "Quantitat": 1
-                },
-                {
-                    "Nom": "Aigua",
-                    "Descripció": "Aigua de manantial",
-                    "Imatge": "www.imatgebeguda2",
-                    "Preu": 2,
-                    "Quantitat": 1
-                } 
-            ],
-            "PreuTotal": 20,
-            "CategoriaComanda": "Domicili",
-            "Repartidor":{
+            {
+                "_id": "2",
                 "Nom": "Maura",
                 "Cognoms": "Amadi Vilanova",
                 "NIF": "45634577S",
                 "Telefon": "456789765",
                 "Posició": "Repartidor"
-            },
-            "Local": {
-                "Nom": "Pizzeria Valken1",
-                "Adreca": {
-                    "Carrer": "Avinguda de Madrid",
-                    "Numero": 78,
-                    "Pis": 0,
-                    "Localitat": "Barcelona",
-                    "Provincia": "Barcelona",
-                    "CP": "08019",
-                    "Pais": "Espana"
-                },
-                "Telefon": "934907643",
-                "NIF": "A12459892"
-            },
-            "Client": {
-                "Nom": "Laura",
-                "Cognoms": "Garriga Vilafont",
-                "Adreca": {
-                    "Carrer": "Avinguda del Cubi",
-                    "Numero": 23,
-                    "Pis": 5,
-                    "Localitat": "Barcelona",
-                    "Provincia": "Barcelona",
-                    "CP": "08017",
-                    "Pais": "Espana"
-                },
-                "Telefon": "934907643"
-            },
-           "ComandaId": "1345"
-        },
-        {
-            "DataComanda": {"$date": "2022-11-26"},
-            "Producte": [
-             {
-                "Nom": "Pizza vegana",
-                "Descripció": "Tomaquet, orenga, xampinyons, alberginies i salsa vegana",
-                "Imatge":"www.imatgepizza3.com",
-                "Preu": 10,
-                "Categoria": "Vegana",
-                "Quantitat": 1
-             },
-             {
-                "Nom": "Aigua",
-                "Descripció": "Aigua de manantial",
-                "Imatge": "www.imatgebeguda2",
-                "Preu": 2,
-                "Quantitat": 1
             }
-            ],
+        ]
+    }
+);
+db.Tendes.insertOne(
+    {
+        "_id": "2",
+        "Nom": "Pizzeria Valken2",
+        "Adreca": {
+            "Carrer": "Viladomat",
+            "Numero": 12,
+            "Pis": 0,
+            "Localitat": "Girona",
+            "Provincia": "Girona",
+            "CP": "17003",
+            "Pais": "Espana"
+        },
+        "Telefon": "934976545",
+        "NIF": "B12089872",
+        "Treballadors": [
+            {
+                "_id": "1",
+                "Nom": "Ismael",
+                "Cognoms": "Gutierrez Guimo",
+                "NIF": "45321566Y",
+                "Telefon": "659348721",
+                "Posicio": "cuiner"
+            },
+            {
+                "_id": "2",
+                "Nom": "Leila",
+                "Cognoms": "Yock Lima",
+                "NIF": "45732400A",
+                "Telefon": "654321976",
+                "Posició": "Repartidor"
+            }
+        ]
+    }
+);
+db.Productes.insertOne(
+    {
+        "_id": "1",
+        "Nom": "Pizza barbacoa",
+        "Descripció": "Tomaquet, orenga, formatge, carn picada i salsa barbacoa",
+        "Imatge": "www.imatgepizza1.com",
+        "Preu": 10,
+        "Categoria": "Carn"
+    }
+);
+db.Productes.insertOne(
+    {
+        "_id": "2",
+        "Nom": "Pizza quatre formatges",
+        "Descripció": "Tomaquet, orenga, formatge blau, de cabra, de mozzarella i gorgonzola",
+        "Imatge": "www.imatgepizza2.com",
+        "Preu": 10,
+        "Categoria": "SenseCarn"
+    }
+);
+db.Productes.insertOne(
+    {
+        "_id": "3",
+        "Nom": "Pizza vegana",
+        "Descripció": "Tomaquet, orenga, xampinyons, alberginies i salsa vegana",
+        "Imatge":"www.imatgepizza3.com",
+        "Preu": 10,
+        "Categoria": "Vegana"
+    }
+);
+db.Productes.insertOne(
+    {
+        "_id": "4",
+        "Nom": "Haburguesa americana",
+        "Descripció": "Carn, formatge, tomaquet, ou ferrat, enciam i maionesa",
+        "Imatge": "www.imatgehamburguesa1.com",
+        "Preu": 8,
+        "Categoria": "Carn"
+    }
+);
+db.Productes.insertOne(
+    {
+        "_id": "5",
+        "Nom": "Hamburguesa vegetariana",
+        "Descripció": "Carn beyond, formatge, tomaquet, enciam i salsa de iogurt",
+        "Imatge": "www.imatgehamburguesa2.com",
+        "Preu": 8,
+        "Categoria": "SenseCarn"
+    }
+);
+db.Productes.insertOne(
+    {
+        "_id": "6",
+        "Nom": "Hamburguesa Vegana",
+        "Descripció": "Carn beyond, tomaquet, enciam, alberginia i salsa vegana",
+        "Imatge": "www.imatgehamburguesa3.com",
+        "Preu": 8,
+        "Categoria": "Vegana"
+    }
+);
+db.Productes.insertOne(
+    {
+        "_id": "7",
+        "Nom": "Coca-cola",
+        "Descripció": "Beguda de cola amb gas",
+        "Imatge": "www.imatgebeguda1",
+        "Preu": 2
+    }
+);
+db.Productes.insertOne(
+    {
+        "_id": "8",
+        "Nom": "Aigua",
+        "Descripció": "Aigua de manantial",
+        "Imatge": "www.imatgebeguda2",
+        "Preu": 2
+    }
+);
+db.Clients.insertOne(
+    {
+        "Nom": "Laura",
+        "Cognoms": "Garriga Vilafont",
+        "Adreca": {
+            "Carrer": "Avinguda del Cubi",
+            "Numero": 23,
+            "Pis": 5,
+            "Localitat": "Barcelona",
+            "Provincia": "Barcelona",
+            "CP": "08017",
+            "Pais": "Espana"
+        },
+        "Telefon": "934907643",
+        "Comandes": {
+            "DataComanda": {"$date": "2022-10-18"},
+            "Productes_id": ["1","8"],
             "PreuTotal": 12,
-            "CategoriaComanda": "Tenda",
-            "Local": {
-                "Nom": "Pizzeria Valken2",
-                "Adreca": {
-                    "Carrer": "Viladomat",
-                    "Numero": 12,
-                    "Pis": 0,
-                    "Localitat": "Girona",
-                    "Provincia": "Girona",
-                    "CP": "17003",
-                    "Pais": "Espana"
-                },
-                "Telefon": "934976545",
-                "NIF": "B12089872"
-            },
-            "Client": {
-                "Nom": "Sergi",
-                "Cognoms": "Viladomat Herrera",
-                "Adreca": {
-                    "Carrer": "Avinguda de Madrid",
-                    "Numero": 78,
-                    "Pis": 0,
-                    "Localitat": "Girona",
-                    "Provincia": "Girona",
-                    "CP": "17005",
-                    "Pais": "Espana"
-                },
-                "Telefon": "639286547"
-            },
-            "ComandaId": "1346"
-         }
-    ]
+            "CategoriaComanda": "Domicili",
+            "Repartidor_id": "2",
+            "Local_id": "1"
+        }
+    }
+);
+db.Clients.insertOne(
+    {
+        "Nom": "Sergi",
+        "Cognoms": "Viladomat Herrera",
+        "Adreca": {
+            "Carrer": "Avinguda de Madrid",
+            "Numero": 78,
+            "Pis": 0,
+            "Localitat": "Girona",
+            "Provincia": "Girona",
+            "CP": "17005",
+            "Pais": "Espana"
+        },
+        "Telefon": "639286547",
+        "Comandes": {
+            "DataComanda": {"$date": "2022-11-26"},
+            "Productes_id": ["4","7"],
+            "PreuTotal": 10,
+            "CategoriaComanda": "Domicili",
+            "Repartidor_id": "2",
+            "Local_id": "2"
+        }
+    }
 );
